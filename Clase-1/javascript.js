3220463e5022a86b06a6765032673c2672428040
JohnnaYoli <johanna.yoli@gmail.com>
2017-06-03 12:07:11 -0300

    Ejercicio Clase 1

---
+++ Clase-1/javascript.js
@@ -0,0 +1,7 @@
+var nombre = prompt("Ingrese su nombre");
+var numuno = parseInt(prompt("Ingrese un numero"));
+var numdos = parseInt(prompt("Ingrese otro numero"));
+var numtres = 20;
+
+
+alert("Bienvenido "+nombre+"\nLa suma de "+numuno+" y "+numtres+" es igual a "+(numuno+numtres)+"."+"\nLa resta de "+numtres+" y "+numuno+" es igual a "+(numtres-numuno)+"."+"\nLa suma de "+numuno+" y "+numdos+" es igual a "+(numuno+numdos)+".");
