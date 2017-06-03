4b9f1b61fd574d41d3859c6d12b1ac554b287082
JohnnaYoli <johanna.yoli@gmail.com>
2017-06-03 14:22:00 -0300

    Ejercicio 2

--- Clase-1/javascript.js
+++ Clase-1/javascript.js
@@ -3,5 +3,4 @@ var numuno = parseInt(prompt("Ingrese un numero"));
 var numdos = parseInt(prompt("Ingrese otro numero"));
 var numtres = 20;
 
-
 alert("Bienvenido "+nombre+"\nLa suma de "+numuno+" y "+numtres+" es igual a "+(numuno+numtres)+"."+"\nLa resta de "+numtres+" y "+numuno+" es igual a "+(numtres-numuno)+"."+"\nLa suma de "+numuno+" y "+numdos+" es igual a "+(numuno+numdos)+".");
---
+++ Clase-2/javascript.js
@@ -0,0 +1,25 @@
+//var respuesta = prompt("Querés seguir viendo la clase?").toLowerCase();
+
+//if(respuesta == "si" || respuesta == "sí") {
+//	alert("Seguimos con la clase!");
+//} 
+//else if (respuesta == "no") {
+//	alert("Adiós!");
+//}
+//else {
+//	alert("No entendí, podrías volver a intentar?");
+//}
+
+//respuesta == "si" ? alert("Seguimos!") : alert("Adiós!");
+
+//if(respuesta) {
+//	alert("Gracias!");
+//}
+
+var nombre = prompt("Cuál es tu nombre?");
+
+if (nombre) {
+	alert("Bienvenido "+nombre);
+} else {
+	alert("Por favor, ingrese su nombre.");
+}
